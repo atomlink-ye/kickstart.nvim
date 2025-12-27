10eb8ae1fa7c8c5fb1e8ce0696dd983efb3a00ea
link.ye <link.ye@bybit.com>
2025-12-27 17:51:25 +0800

    update
    - add shortcut for markdown preview
    - remove conflicts for opencode

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -122,8 +122,8 @@ return {
       -- Recommended/example keymaps.
       vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end,
         { desc = "Ask opencode" })
-      vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
-        { desc = "Execute opencode action…" })
+      -- vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,
+      --   { desc = "Execute opencode action…" })
       vim.keymap.set({ "n", "x" }, "ga", function() require("opencode").prompt("@this") end, { desc = "Add to opencode" })
       vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end, { desc = "Toggle opencode" })
       vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,
--- lua/custom/plugins/util/markdown.lua
+++ lua/custom/plugins/util/markdown.lua
@@ -3,6 +3,12 @@ return {
   lazy = true,
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
+  keys = {
+    { "<leader>OM", "<Cmd>MarkdownPreview<CR>", desc = "[O]pen [M]arkdown Preview" },
+  },
+  init = function()
+    vim.g.mkdp_auto_close = 0
+  end,
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
